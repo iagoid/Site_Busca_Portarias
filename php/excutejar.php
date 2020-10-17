4984dab50d8246a0dc0b77a8a3e326274c7fef88
iagoid <iagoid01@gmail.com>
2020-10-17 14:49:29 -0300

    Conversão json

--- php/excutejar.php
+++ php/excutejar.php
@@ -1,23 +1,24 @@
 <?php
 
-$palavra = isset($_POST['wrdConsult']) ? $palavra = $_POST['wrdConsult'] : $palavra = '';
+$palavra = isset($_POST['wrdConsult']) ? $palavra = $_POST['wrdConsult'] : $palavra = 'silva';
 
 if ($palavra != null) {
 	unset($arrayDados);
 	$arrayDados = array();
 	$shell = exec('java -jar "C:\Users\Igor\Documents\NetBeansProjects\PROJETO_BUSCADOR-PORTARIAS-\Buscador_Portarias\dist\Buscador_Portarias.jar" -query "'.trim($palavra).'"' , $saida);
 	$quantidade = count($saida);
-	// var_dump(count($saida));
+	var_dump($saida);
 	
 	for ($i=0; $i < $quantidade; $i++) {
-		$json = json_decode(utf8_encode($saida[$i]));
-		// var_dump($json);
+		// var_dump($saida[$i]);
+		
+		$json = json_decode(mb_convert_encoding($saida[$i], "UTF-8", "auto"));
+		var_dump($json);
 		// var_dump(utf8_encode($saida[1]));
 		// echo"<br><br>";
 		// exit();
 		// die();
 		$jsonCount = (is_array($json) ? count($json) : 0);
-
 		if ($jsonCount > 0) {
 			foreach ($json as $key => $value) {	
 				$arrayDados[$key]['datePort'] = str_replace("\/", "-", $value->datePort) ;
@@ -28,6 +29,7 @@ if ($palavra != null) {
 				}
 				// $arrayDados['numPort'][] = $value->numPort;
 				$arrayDados[$key]['nameDoc'] = $value->nameDoc;
+				// $arrayDados[$key]['conteudo'] = $value->conteudo;
 			}
 		}
 	}
